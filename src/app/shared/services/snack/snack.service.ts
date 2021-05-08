b0d4b702cd98773d6f77223ccf48b670bd0d0145
Ricard Mallafre <rikitzzz@gmail.com>
2021-05-09 00:37:40 +0200

    fix: not using tap anymore

--- src/app/shared/services/snack/snack.service.ts
+++ src/app/shared/services/snack/snack.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class SnackService {
   constructor(private snackBar: MatSnackBar, private router: Router) {}
 
   authError() {
     const openedSnack = this.snackBar.open('You must be logged in!', 'OK', {
-      duration: 5000,
+      duration: 5000
     });
 
-    return openedSnack
-      .onAction()
-      .pipe(tap((_) => this.router.navigate(['/user/login'])))
-      .subscribe();
+    return openedSnack.onAction().subscribe(() => this.router.navigate(['/user/login']));
   }
 }
